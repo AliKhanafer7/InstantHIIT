94c2bf18bddaf4a8f6aea080baa17b4f6aa093ca
Ali Khanafer <ali.h.khanafer@gmail.com>
2020-12-29 12:53:55 -0500

    Initial Commit

---
+++ InstantHIIT/InstantHIITApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantHIITApp.swift
+//  InstantHIIT
+//
+//  Created by Ali Khanafer on 2020-12-29.
+//
+
+import SwiftUI
+
+@main
+struct InstantHIITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
